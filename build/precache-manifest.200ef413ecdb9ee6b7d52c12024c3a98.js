ff71905b0a34742731bbbe9df9b7e74f5005a4ec
Johanna Rousi <johanna.rousi@gmail.com>
2019-07-12 18:22:34 +0200

    added frontend, build file

---
+++ build/precache-manifest.200ef413ecdb9ee6b7d52c12024c3a98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dffb7c9d6b6d074a4beb9649ac84170",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d284f77c4762c4786171",
+    "url": "/static/css/main.425fc9d1.chunk.css"
+  },
+  {
+    "revision": "5639b38e2899665f4bb3",
+    "url": "/static/js/2.c834f1ab.chunk.js"
+  },
+  {
+    "revision": "d284f77c4762c4786171",
+    "url": "/static/js/main.f4da8fb5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
